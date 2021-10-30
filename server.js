4d10c4b12af1186995ee02c4d7eaa6ff24837bad
Santiago Meneses <santiagome2014@hotmail.com>
2021-10-30 15:54:02 -0500

    Fixeds

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const history = require('connect-history-api-fallback');
+var history = require('connect-history-api-fallback');
 const serveStatic = require("serve-static")
-
 const path = require('path');
-app = express();
 
-app.use(history({
-    index: '/' //whatever your home/index path is default is /index.html
-}));
+var app = express();
+
+app.use(history());
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
